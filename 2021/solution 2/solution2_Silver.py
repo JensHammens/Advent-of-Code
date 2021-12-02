74ee9882265b05a649fb269446bb4de67c59ec10
JensHammens <hammensjens@gmail.com>
2021-12-02 18:11:54 +0100

    Add day2 silver

---
+++ 2021/solution 2/solution2_Silver.py
@@ -0,0 +1,20 @@
+import os
+
+location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+input = open(os.path.join(location,'solutiondata.csv')).read()
+lines = input.split("\n")
+
+depth = 0
+horizontal = 0
+
+for line in lines:
+    parts = line.split(" ")
+    direction = parts[0]
+    if(direction == "forward"):
+        horizontal+=int(parts[1])
+    if(direction == "down"):
+        depth+=int(parts[1])
+    if(direction == "up"):
+        depth-=int(parts[1])
+position = depth * horizontal
+print(position)
