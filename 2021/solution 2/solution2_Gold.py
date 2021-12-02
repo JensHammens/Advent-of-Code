99954063e0f5bdfb66ee5c932edd0e56f91483cd
JensHammens <hammensjens@gmail.com>
2021-12-02 18:24:28 +0100

    Add day2 Gold

---
+++ 2021/solution 2/solution2_Gold.py
@@ -0,0 +1,22 @@
+import os
+
+location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+input = open(os.path.join(location,'solutiondata.csv')).read()
+lines = input.split("\n")
+
+depth = 0
+horizontal = 0
+aim = 0
+
+for line in lines:
+    parts = line.split(" ")
+    direction = parts[0]
+    if(direction == "forward"):
+        horizontal+=int(parts[1])
+        depth += aim*int(parts[1])
+    if(direction == "down"):
+        aim+=int(parts[1])
+    if(direction == "up"):
+        aim-=int(parts[1])
+position = depth * horizontal
+print(position)
